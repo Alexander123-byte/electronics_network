9bea385627aed1fa1551e5ab03f0d685d0e9ae2e
Alexander <san.juckov2017@yandex.ru>
2026-02-12 22:47:49 +0300

    Bug fixes

--- network/views.py
+++ network/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets, status
+# network/views.py
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from .models import NetworkNode
@@ -34,33 +34,35 @@ class NetworkNodeViewSet(viewsets.ModelViewSet):
             return NetworkNodeCreateUpdateSerializer
         return NetworkNodeSerializer
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=['get'], url_path='factories')
     def factories(self, request):
         """Получение всех заводов (уровень 0)"""
-        factories = self.queryset.filter(level=0)
+        factories = self.get_queryset().filter(level=0)
         serializer = self.get_serializer(factories, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'])
-    def retails(self, request):
+    @action(detail=False, methods=['get'], url_path='retailers')
+    def retailers(self, request):
         """Получение всех розничных сетей (уровень 1)"""
-        retails = self.queryset.filter(level=1)
-        serializer = self.get_serializer(retails, many=True)
+        retailers = self.get_queryset().filter(level=1)
+        serializer = self.get_serializer(retailers, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=['get'], url_path='entrepreneurs')
     def entrepreneurs(self, request):
         """Получение всех ИП (уровень 2)"""
-        entrepreneurs = self.queryset.filter(level=2)
+        entrepreneurs = self.get_queryset().filter(level=2)
         serializer = self.get_serializer(entrepreneurs, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'])
+    @action(detail=True, methods=['get'], url_path='debt-info')
     def debt_info(self, request, pk=None):
         """Получение информации о задолженности"""
         node = self.get_object()
         return Response({
             'node_name': node.name,
             'debt': node.debt,
-            'supplier': node.supplier.name if node.supplier else None
+            'supplier': node.supplier.name if node.supplier else None,
+            'level': node.level,
+            'level_display': node.get_level_display_name()
         })
